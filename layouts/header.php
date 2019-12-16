ba0767ff8fbed4a84543dbb93c20ffca36b94888
Morgan <morgan.lexter@gmail.com>
2019-12-16 18:47:53 +0100

    Ajust CSS listPosts

--- layouts/header.php
+++ layouts/header.php
@@ -1,19 +1,35 @@
-<div id="header_container">
-    <div id="logo_site_container">
-        <div id="img_logo">
-        </div>
-        <div id="JF_text_logo_container">
-            <h1 id="logo_site_name">JEAN FORTEROCHE</h1>
-            <h2 id="sub_logo_title">Le blog de l'auteur</h2>
-        </div>
-    </div>
+<header>
+    <nav class="navbar navbar-expand-lg">
+        <a class="navbar-brand" href="<?= HOST ?>">
+            <div id="logo_site_container">
 
-    <div id="menu_container">
-        <ul id="main-menu">
-            <li class="item-menu-header"><a href="<?php echo HOST; ?>">Accueil</a></li>
-            <li class="item-menu-header"><a href="<?php echo HOST; ?>book">Livres</a></li>
-            <li class="item-menu-header"><a href="<?php echo HOST; ?>about">A propos</a></li>
-            <li class="item-menu-header"><a href="<?php echo HOST; ?>contact">Contact</a></li>
-        </ul>
-    </div>
-</div>
+                <div id="JF_text_logo_container">
+                    <h1 id="logo_site_name">JEAN FORTEROCHE</h1>
+                    <h2 id="sub_logo_title">Le blog de l'auteur</h2>
+                </div>
+            </div>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-lg-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= HOST . '' ?>">Accueil<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= HOST . 'book' ?>">Livre</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= HOST . 'about' ?>">A propos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= HOST . 'contact' ?>">Contact</a>
+                </li>
+            </ul>
+
+        </div>
+    </nav>
+
+</header>
