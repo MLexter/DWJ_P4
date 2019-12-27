a90235168b4d8e424d51188e355ddef9cdd0d5ee
Morgan <morgan.lexter@gmail.com>
2019-12-27 17:49:23 +0100

    Finalising CSS on Connexion Screen

--- view/connexionView.php
+++ view/connexionView.php
@@ -1,7 +1,7 @@
 <?php $title_content = 'Connexion à l\'espace d\'administration'; ?>
 
 <div id="main-connexion-screen" class="container-fluid d-flex align-items-center">
-    <div id="connexion_container" class="container d-flex align-items-center shadow border bg-white rounded col-sm-12">
+    <div id="connexion_container" class="container col-md-8 shadow border bg-white rounded ">
         <div id="connexion_form">
             <h1 class="text-center">Connectez-vous à votre espace:</h1>
             <br />
@@ -10,17 +10,17 @@
                 <div class="form-group">
 
                     <table class="table">
-                        <tr>
+                        <tr class="inputs_connexion_responsive">
                             <td><label for="user_id" class="connexion_labels">IDENTIFIANT :</label></td>
                             <td><input type="text" name="ID_user" class="form-control connexion_inputs" placeholder="Saisissez votre identifiant"></td>
                         </tr>
-                        <tr>
+                        <tr class="inputs_connexion_responsive">
                             <td><label for="password_user" class="connexion_labels">MOT DE PASSE :</label></td>
                             <td><input type="password" name="password_user" class="form-control connexion_inputs" placeholder="Tapez votre mot de passe"></td>
                         </tr>
 
                     </table>
-                    <div class="container-fluid d-flex justify-content-end">
+                    <div id="container-connexion-btn" class="container-fluid d-flex justify-content-end">
                         <input type="submit" class="btn btn-primary" name="submit_connexion" value="Connexion">
                     </div>
 
