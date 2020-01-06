c1544d392b9c62f2708fbe18ef87bffb22e6b960
Morgan <morgan.lexter@gmail.com>
2020-01-06 15:47:21 +0100

    CSS ajusts ALL OK + 404

--- classes/Router.php
+++ classes/Router.php
@@ -55,7 +55,7 @@ class Router
             $currentController = new $controller();
             $currentController->$method();
         } else {
-            echo '404';
+            header('Location: ' . HOST . '404.php');
         }
     }
 }
